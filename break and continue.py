fc4ecbbcbb0c9e447588fa35786f3e949694a115
MAX <108987965+Max6969420@users.noreply.github.com>
2022-07-21 12:57:14 +0530

    Add files via upload

---
+++ break and continue.py
@@ -0,0 +1,21 @@
+# i = 0
+# while(True):
+#     if i + 1<5:
+#         i = i + 1
+#         continue
+#     print(i+1, end=" ")
+#     if(i==44):
+#         break #Stops the loop
+#     i = i +1
+# i = 0
+# while(True):
+#     if i +1>100:
+#         break
+#         print("Congrats")
+#     print(i=1, end=" ")
+# if(1==)
+#     continue
+
+
+
+
---
+++ looper_1.py
@@ -0,0 +1,9 @@
+while("True"):
+    print("Enter a number greater than 100")
+    num = int(input())
+    if num<=100:
+        print("Try again bruh!")
+    else:
+        print("Congrats")
+        print("Thanks for using Looper_1 by Max")
+        break
