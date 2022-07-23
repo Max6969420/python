8a5a7f7c3be3c0d6c6a3b8737a93dd7c149fbc49
MAX <108987965+Max6969420@users.noreply.github.com>
2022-07-23 13:46:29 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,85 @@
+# There are 7 types of operators:
+
+# 1. Arithmetic operators
+
+# 2. Assignment operators
+
+# 3. Comparison operators
+
+# 4. Logical operators
+
+# 5. Identity operators
+
+# 6. Membership operators
+
+# 7. Bitwise operators
+# ----------------------
+
+# 1. Arithmetic operators:
+
+"""
+print("5 + 6 is", 5 + 6)
+print("5 - 6 is", 5 - 6)
+print("5 / 6 is", 5 / 6)
+print("5 ** 6 is", 5 ** 6)
+print("15 // 6 is", 15 // 6)
+
+"""
+
+# 2. Assignment operators:
+"""
+x = 5
+print(x)
+x += 7
+print(x)
+x -= 3
+print(x)
+x %=2
+print(x)
+"""
+
+# 3. Comparison operators:
+i = 5
+"""
+print(i == 5) #true
+print(i <= 5) #true
+print(i >= 5) #true
+print(i != 5) #false (!= is not equal's symbol)
+print(i > 5) #false
+print(i < 5) #false
+"""
+# 4. Logical operators:
+a = True
+b = False
+"""
+print(a or b)# True prints (True>False)
+print(a and b)# Both should have been true, SO it's False
+print(a and a)# True
+print(b and b)# False
+"""
+# 5. Identity operators:
+"""
+print(5 is not 5) # False
+print(5 is 5) # True
+print(5 is 6) # False
+print(6 is not 5) # True
+"""
+# 6. Membership operators:
+"""
+list = [2, 4, 5.9, 38,7123897,333,12,69,6942]
+print(69420 not in list) #True
+print(69 in list) #True
+print(69 not in list)#False
+"""
+# 7. Bitwise operators:
+#Binary
+"""
+0 = 00
+1 = 01
+2 = 10
+3 = 11
+"""
+"""
+print(0 or 1)#binary of 1
+print(1 | 3)# or = |
+"""
